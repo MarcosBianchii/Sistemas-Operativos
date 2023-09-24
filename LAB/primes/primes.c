ed498e7e51cf71111de4c753b7dd1befcfc02463
Marcos Bianchi <marcobianchi07@gmail.com>
2023-09-24 14:23:02 -0300

    primes

--- LAB/primes/primes.c
+++ LAB/primes/primes.c
@@ -4,6 +4,24 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+// Escribe en fd1 los números no
+// divisibles por p leidos de fd0.
+int
+escribir_no_divisibles(int p, int fd0, int fd1)
+{
+	int num;
+	while (read(fd0, &num, sizeof(num)) > 0) {
+		if (num % p != 0) {
+			if (write(fd1, &num, sizeof(num)) == -1) {
+				perror("Error escribiendo en pipe");
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
 int
 primes(const int izq_read)
 {
@@ -12,7 +30,6 @@ primes(const int izq_read)
 	// izquierdo. En caso de que no haya
 	// más primos termina el proceso.
 	if (read(izq_read, &primo, sizeof(primo)) <= 0) {
-		close(izq_read);
 		return 0;
 	}
 
@@ -21,7 +38,6 @@ primes(const int izq_read)
 	int der_fds[2];
 	if (pipe(der_fds) == -1) {
 		perror("Error creando pipe");
-		close(izq_read);
 		return 1;
 	}
 
@@ -35,41 +51,44 @@ primes(const int izq_read)
 
 		// Volver a correr primes
 		// con el pipe derecho.
-		if (primes(der_fds[0]) == 1) {
-			close(der_fds[0]);
-			return 1;
-		}
-
+		int res = primes(der_fds[0]);
 		close(der_fds[0]);
+		return res;
 
 	} else if (i > 0) {
 		// PADRE
 		close(der_fds[0]);
 
-		int num;
-		// Filtra los múltiplos del primo y
-		// escribe los "primos" en el pipe derecho.
-		while (read(izq_read, &num, sizeof(num)) > 0)
-			if (num % primo != 0)
-				if (write(der_fds[1], &num, sizeof(num)) == -1) {
-					perror("Error escribiendo en pipe");
-					close(der_fds[1]);
-					close(izq_read);
-					wait(NULL);
-					return 1;
-				}
-
+		// Pasar los números no divisibles
+		// por primo al pipe derecho.
+		int res = escribir_no_divisibles(primo, izq_read, der_fds[1]);
 		close(der_fds[1]);
-		close(izq_read);
-		wait(NULL);
+
+		int status;
+		// Conseguir el estado
+		// de finalización del hijo.
+		waitpid(-1, &status, 0);
+		return res == 1 ? 1 : WEXITSTATUS(status);
 
 	} else {
 		// ERROR
 		perror("Error en fork");
 		close(der_fds[0]);
 		close(der_fds[1]);
-		close(izq_read);
 		return 1;
+	}
+}
+
+// Escribe el rango de
+// números en fd1.
+int
+escribir_rango(int N, int fd1)
+{
+	for (int i = 2; i <= N; i++) {
+		if (write(fd1, &i, sizeof(i)) == -1) {
+			perror("Error escribiendo en pipe");
+			return 1;
+		}
 	}
 
 	return 0;
@@ -95,16 +114,29 @@ main(int argc, char *argv[])
 		return 1;
 	}
 
-	// Escribir el rango de
-	// números en el pipe.
-	for (int i = 2; i <= N; i++)
-		if (write(fds[1], &i, sizeof(i)) == -1) {
-			perror("Error escribiendo en pipe");
-			close(fds[0]);
-			close(fds[1]);
-			return 1;
-		}
+	int i = fork();
+	if (i == 0) {
+		// HIJO
+		close(fds[1]);
+		int res = primes(fds[0]);
+		close(fds[0]);
+		return res;
 
-	close(fds[1]);
-	return primes(fds[0]);
+	} else if (i > 0) {
+		// PADRE
+		int res = escribir_rango(N, fds[1]);
+		close(fds[0]);
+		close(fds[1]);
+
+		int status;
+		// Conseguir el estado
+		// de finalización del hijo.
+		waitpid(-1, &status, 0);
+		return res == 1 ? 1 : WEXITSTATUS(status);
+
+	} else {
+		// ERROR
+		perror("Error en fork");
+		return 1;
+	}
 }
